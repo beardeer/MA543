7358186b7b74656553eb3be08f7b00967da80ae1
Xiaolu Xiong <beardeer@gmail.com>
2016-03-18 14:07:11 -0400

    update

--- hw3_q2.r
+++ hw3_q2.r
@@ -1,14 +1,14 @@
-y = 3
-lambda = 2
+y = 2
+lambda = 4
 beta = seq(-10, 10, 0.1)
-plot(beta, (y - beta)^2 + lambda * beta^2, pch = 20, xlab = "beta", ylab = "Ridge optimization")
+plot(beta, (y - beta)^2 + lambda * beta^2, pch = 20, xlab = "beta", ylab = "Ridge")
 beta.est = y / (1 + lambda)
 points(beta.est, (y - beta.est)^2 + lambda * beta.est^2, col = "red", pch = 4, lwd = 5)
 
 
-y = 3
-lambda = 2
+y = 2
+lambda = 4
 beta <- seq(-10, 10, 0.1)
-plot(beta, (y - beta)^2 + lambda * abs(beta), pch = 20, xlab = "beta", ylab = "Lasso optimization")
+plot(beta, (y - beta)^2 + lambda * abs(beta), pch = 20, xlab = "beta", ylab = "Lasso")
 beta.est = y - lambda / 2
 points(beta.est, (y - beta.est)^2 + lambda * abs(beta.est), col = "red", pch = 4, lwd = 5)
